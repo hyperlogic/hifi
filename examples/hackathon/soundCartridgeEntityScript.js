4206942b61dbc84201938423d0df1d5cc99975ff
ericrius1 <ericrius1>
2016-03-02 18:43:06 -0800

    add mapping for sound volume based on closest hand's distance to cartridge

--- examples/hackathon/soundCartridgeEntityScript.js
+++ examples/hackathon/soundCartridgeEntityScript.js
@@ -1,13 +1,22 @@
-
 (function() {
     Script.include("../libraries/utils.js");
     var _this;
     SoundCartridge = function() {
         _this = this;
-        _this.audioOptions = {loop: true};
-        _this.playingColor = {red: 0, green: 200, blue: 10};
-        _this.notPlayingColor = {red: 200, green: 0, blue: 0};
-     
+        _this.audioOptions = {
+            loop: true
+        };
+        _this.playingColor = {
+            red: 0,
+            green: 200,
+            blue: 10
+        };
+        _this.notPlayingColor = {
+            red: 200,
+            green: 0,
+            blue: 0
+        };
+
     };
 
     SoundCartridge.prototype = {
@@ -16,21 +25,44 @@
             var position = Entities.getEntityProperties(_this.entityID, "position").position;
             if (!_this.injector) {
                 _this.audioOptions.position = position;
-                _this.injector = Audio.playSound(_this.clip, _this.audioOptions);  
+                _this.injector = Audio.playSound(_this.clip, _this.audioOptions);
             } else {
                 //We already have injector so just restart
                 _this.injector.restart();
             }
 
-            Entities.editEntity(_this.entityID, {color: _this.playingColor});
-        }, 
+            Entities.editEntity(_this.entityID, {
+                color: _this.playingColor
+            });
+        },
 
         stopSound: function() {
             print("EBL STOP SOUND!!!!!!!!!!!!!!")
             if (_this.injector) {
                 _this.injector.stop();
-                Entities.editEntity(_this.entityID, {color: _this.notPlayingColor});
+                Entities.editEntity(_this.entityID, {
+                    color: _this.notPlayingColor
+                });
             }
+        },
+
+        updateSoundPosition: function() {
+            print("CARTRIDGE POSITION MAY HAVE CHANGED, UPDATE POSITION!");
+            if (_this.injector) {  
+              _this.position = Entities.getEntityProperties(_this.entityID, "position").position;
+              _this.audioOptions.position = _this.position;
+              _this.injector.setOptions(_this.audioOptions);
+            }
+
+        },
+
+        releaseGrab: function() {
+            _this.updateSoundPosition();
+
+        },
+
+        releaseEquip: function() {
+                _this.updateSoundPosition();
         },
 
         preload: function(entityID) {
@@ -49,7 +81,7 @@
             }
         }
 
-   
+
     };
 
     // entity scripts always need to return a newly constructed object of our type
